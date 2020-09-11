5c63abc3c986c57e191b3b8e78ba6ed50bdf274f
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-11 14:53:56 +1000

    Added normalize to improve user agent consistencies

--- src/App.tsx
+++ src/App.tsx
@@ -8,57 +8,14 @@ function App() {
 
       </header>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-            <div className="col-xs-1"><span>1</span></div>
-          </div>
-          <div className="row">
-            <div className="col-xs-2"><span>2</span></div>
-            <div className="col-xs-2"><span>2</span></div>
-            <div className="col-xs-2"><span>2</span></div>
-            <div className="col-xs-2"><span>2</span></div>
-            <div className="col-xs-2"><span>2</span></div>
-            <div className="col-xs-2"><span>2</span></div>
-          </div>
-          <div className="row">
-            <div className="col-xs-3"><span>3</span></div>
-            <div className="col-xs-3"><span>3</span></div>
-            <div className="col-xs-3"><span>3</span></div>
-            <div className="col-xs-3"><span>3</span></div>
-          </div>
-          <div className="row">
-            <div className="col-xs-4"><span>4</span></div>
-            <div className="col-xs-4"><span>4</span></div>
-            <div className="col-xs-4"><span>4</span></div>
-          </div>
-          <div className="row">
-            <div className="col-xs-6"><span>6</span></div>
-            <div className="col-xs-6"><span>6</span></div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12"><span>12</span></div>
-          </div>
-        </div>
-
-
-
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
-          <div className="col-xs-6">6</div>
-          <div className="col-xs-offset-3 col-xs-3">3</div>
+          <div className="col-xs-4"><span>4</span></div>
+          <div className="col-xs-4"><span>4</span></div>
+          <div className="col-xs-4"><span>4</span></div>
         </div>
       </div>
+
     </div>
 
   );
