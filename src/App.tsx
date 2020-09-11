7d3b3108efc52195b39f5b1e865b591b5b3f249c
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-11 17:48:47 +1000

    Added awesome new styles for input fields (radio, toggle, checkbox)

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,72 @@ function App() {
 
       </header>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-xs-4"><span>4</span></div>
-          <div className="col-xs-4"><span>4</span></div>
-          <div className="col-xs-4"><span>4</span></div>
-        </div>
-      </div>
+          <div className="container">
+              <div className="checkbox">
+                  <p className="title">
+                      Custom checkbox
+		</p>
+                  <label htmlFor="customCheckbox1" className="checkbox__label">
+                      <div className="checkbox__group">
+                          <input type="checkbox" className="checkbox__input" id="customCheckbox1" hidden />
+                          <span className="checkbox__checked"></span>
+                      </div>
+                  </label>
+
+                  <label htmlFor="customCheckbox2" className="checkbox__label">
+                      <div className="checkbox__group">
+                          <input type="checkbox" className="checkbox__input" id="customCheckbox2" hidden />
+                          <span className="checkbox__checked"></span>
+                      </div>
+                  </label>
+
+                  <label htmlFor="customCheckbox3" className="checkbox__label">
+                      <div className="checkbox__group">
+                          <input type="checkbox" className="checkbox__input" id="customCheckbox3" hidden />
+                          <span className="checkbox__checked"></span>
+                      </div>
+                  </label>
+              </div>
+
+              <div className="radio">
+                  <p className="title">
+                      Custom radio
+		</p>
+                  <label htmlFor="customRadio1" className="radio__label">
+                      <div className="radio__group">
+                          <input type="radio" className="radio__input" id="customRadio1" name="customRadio" hidden />
+                          <span className="radio__checked"></span>
+                      </div>
+                  </label>
+
+                  <label htmlFor="customRadio2" className="radio__label">
+                      <div className="radio__group">
+                          <input type="radio" className="radio__input" id="customRadio2" name="customRadio" hidden />
+                          <span className="radio__checked"></span>
+                      </div>
+                  </label>
+
+                  <label htmlFor="customRadio3" className="radio__label">
+                      <div className="radio__group">
+                          <input type="radio" className="radio__input" id="customRadio3" name="customRadio" hidden />
+                          <span className="radio__checked"></span>
+                      </div>
+                  </label>
+              </div>
+
+              <div className="toggle">
+                  <p className="title">
+                      Custom toggle
+		</p>
+                  <label htmlFor="customToggle" className="toggle__label">
+                      <input type="checkbox" className="toggle__input" id="customToggle" hidden />
+                      <div className="toggle__group">
+                          <span className="toggle__checked"></span>
+                      </div>
+                  </label>
+              </div>
+          </div>
+
 
     </div>
 
