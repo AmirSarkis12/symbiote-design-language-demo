6c686f344bca2dfd021a3f3105143cfabaf11fc8
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-11 14:44:47 +1000

    Added more Symbiote styles + added grid styles based off Aus Gov Design System

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/main.scss';
 
 function App() {
   return (
@@ -6,7 +7,60 @@ function App() {
       <header className="App-header">
 
       </header>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+            <div className="col-xs-1"><span>1</span></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-2"><span>2</span></div>
+            <div className="col-xs-2"><span>2</span></div>
+            <div className="col-xs-2"><span>2</span></div>
+            <div className="col-xs-2"><span>2</span></div>
+            <div className="col-xs-2"><span>2</span></div>
+            <div className="col-xs-2"><span>2</span></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-3"><span>3</span></div>
+            <div className="col-xs-3"><span>3</span></div>
+            <div className="col-xs-3"><span>3</span></div>
+            <div className="col-xs-3"><span>3</span></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-4"><span>4</span></div>
+            <div className="col-xs-4"><span>4</span></div>
+            <div className="col-xs-4"><span>4</span></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-6"><span>6</span></div>
+            <div className="col-xs-6"><span>6</span></div>
+          </div>
+          <div className="row">
+            <div className="col-xs-12"><span>12</span></div>
+          </div>
+        </div>
+
+
+
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-6">6</div>
+          <div className="col-xs-offset-3 col-xs-3">3</div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
