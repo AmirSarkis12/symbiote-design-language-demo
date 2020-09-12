880ae4cd43344ae7900023f882921864bcec2c09
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-12 17:19:04 +1000

    Added a really cool accordion to styles

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,25 @@ function App() {
               </div>
           </div>
 
+          <div className="accordion">
+              <div className="tab">
+                  <input type="checkbox" id="chck1"></input>
+                      <label className="tab-label" htmlFor="chck1">Item 1</label>
+                      <div className="tab-content">
+                          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, reiciendis!
+                    </div>
+
+      </div>
+                  <div className="tab">
+                  <input type="checkbox" id="chck2"></input>
+                          <label className="tab-label" htmlFor="chck2">Item 2</label>
+                          <div className="tab-content">
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!
+                        </div>
+
+      </div>
+                  </div>
+
 
     </div>
 
