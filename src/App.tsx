efb6357df110991b1943a9af77eef9b8f2d00310
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-13 21:40:26 +1000

    Added table styles + added some MUCH needed indentation and structure to sandpit in App.tsx + other style tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -2,101 +2,148 @@ import React from 'react';
 import './styles/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    return (
+        <div className="App">
+            <header className="App-header"></header>
 
-      </header>
+            {/* Checkbox, Radio and toggle */}
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-6">
+                        {/* Checkbox */}
+                        <div className="checkbox">
+                            <p className="title">Custom checkbox</p>
+                            <label htmlFor="customCheckbox1" className="checkbox__label">
+                                <div className="checkbox__group">
+                                    <input type="checkbox" className="checkbox__input" id="customCheckbox1" hidden />
+                                    <span className="checkbox__checked"></span>
+                                </div>
+                            </label>
 
-          <div className="container">
-              <div className="checkbox">
-                  <p className="title">
-                      Custom checkbox
-		</p>
-                  <label htmlFor="customCheckbox1" className="checkbox__label">
-                      <div className="checkbox__group">
-                          <input type="checkbox" className="checkbox__input" id="customCheckbox1" hidden />
-                          <span className="checkbox__checked"></span>
-                      </div>
-                  </label>
+                            <label htmlFor="customCheckbox2" className="checkbox__label">
+                                <div className="checkbox__group">
+                                    <input type="checkbox" className="checkbox__input" id="customCheckbox2" hidden />
+                                    <span className="checkbox__checked"></span>
+                                </div>
+                            </label>
 
-                  <label htmlFor="customCheckbox2" className="checkbox__label">
-                      <div className="checkbox__group">
-                          <input type="checkbox" className="checkbox__input" id="customCheckbox2" hidden />
-                          <span className="checkbox__checked"></span>
-                      </div>
-                  </label>
-
-                  <label htmlFor="customCheckbox3" className="checkbox__label">
-                      <div className="checkbox__group">
-                          <input type="checkbox" className="checkbox__input" id="customCheckbox3" hidden />
-                          <span className="checkbox__checked"></span>
-                      </div>
-                  </label>
-              </div>
-
-              <div className="radio">
-                  <p className="title">
-                      Custom radio
-		</p>
-                  <label htmlFor="customRadio1" className="radio__label">
-                      <div className="radio__group">
-                          <input type="radio" className="radio__input" id="customRadio1" name="customRadio" hidden />
-                          <span className="radio__checked"></span>
-                      </div>
-                  </label>
-
-                  <label htmlFor="customRadio2" className="radio__label">
-                      <div className="radio__group">
-                          <input type="radio" className="radio__input" id="customRadio2" name="customRadio" hidden />
-                          <span className="radio__checked"></span>
-                      </div>
-                  </label>
-
-                  <label htmlFor="customRadio3" className="radio__label">
-                      <div className="radio__group">
-                          <input type="radio" className="radio__input" id="customRadio3" name="customRadio" hidden />
-                          <span className="radio__checked"></span>
-                      </div>
-                  </label>
-              </div>
-
-              <div className="toggle">
-                  <p className="title">
-                      Custom toggle
-		</p>
-                  <label htmlFor="customToggle" className="toggle__label">
-                      <input type="checkbox" className="toggle__input" id="customToggle" hidden />
-                      <div className="toggle__group">
-                          <span className="toggle__checked"></span>
-                      </div>
-                  </label>
-              </div>
-          </div>
-
-          <div className="accordion">
-              <div className="tab">
-                  <input type="checkbox" id="chck1"></input>
-                      <label className="tab-label" htmlFor="chck1">Item 1</label>
-                      <div className="tab-content">
-                          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, reiciendis!
-                    </div>
-
-      </div>
-                  <div className="tab">
-                  <input type="checkbox" id="chck2"></input>
-                          <label className="tab-label" htmlFor="chck2">Item 2</label>
-                          <div className="tab-content">
-                              Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!
+                            <label htmlFor="customCheckbox3" className="checkbox__label">
+                                <div className="checkbox__group">
+                                    <input type="checkbox" className="checkbox__input" id="customCheckbox3" hidden />
+                                    <span className="checkbox__checked"></span>
+                                </div>
+                            </label>
                         </div>
 
-      </div>
-                  </div>
+                        {/* Radio Buttons */}
+                        <div className="radio">
+                            <p className="title">Custom radio</p>
+                            <label htmlFor="customRadio1" className="radio__label">
+                                <div className="radio__group">
+                                    <input type="radio" className="radio__input" id="customRadio1" name="customRadio" hidden />
+                                    <span className="radio__checked"></span>
+                                </div>
+                            </label>
 
+                            <label htmlFor="customRadio2" className="radio__label">
+                                <div className="radio__group">
+                                    <input type="radio" className="radio__input" id="customRadio2" name="customRadio" hidden />
+                                    <span className="radio__checked"></span>
+                                </div>
+                            </label>
 
-    </div>
+                            <label htmlFor="customRadio3" className="radio__label">
+                                <div className="radio__group">
+                                    <input type="radio" className="radio__input" id="customRadio3" name="customRadio" hidden />
+                                    <span className="radio__checked"></span>
+                                </div>
+                            </label>
+                        </div>
 
-  );
+                        {/* Toggle */}
+                        <div className="toggle">
+                            <p className="title">Custom toggle</p>
+                            <label htmlFor="customToggle" className="toggle__label">
+                                    <input type="checkbox" className="toggle__input" id="customToggle" hidden />
+                                    <div className="toggle__group">
+                                        <span className="toggle__checked"></span>
+                                    </div>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+                <div className="row">
+                    <div className="col-xs-6">
+                        {/* Accordion */}
+                        <div className="accordion">
+                            <div className="tab">
+                                <input type="checkbox" id="chck1"></input>
+                                <label className="tab-label" htmlFor="chck1">Item 1</label>
+                                <div className="tab-content">
+                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum, reiciendis!</p>
+                                </div>
+                            </div>
+                            <div className="tab">
+                                <input type="checkbox" id="chck2"></input>
+                                <label className="tab-label" htmlFor="chck2">Item 2</label>
+                                <div className="tab-content">
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+                <div className="row">
+                    <div className="col-xs-12">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Phone</th>
+                                    <th>Email</th>
+                                    <th>Date of Birth</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Amir</td>
+                                    <td>Sarkis</td>
+                                    <td>(03) 9999 9999</td>
+                                    <td><a href="mailto:x@x.com">email@emailaddress.com</a></td>
+                                    <td>12/12/1991</td>
+                                </tr>
+                                <tr>
+                                    <td>John</td>
+                                    <td>Doe</td>
+                                    <td>(02) 8888 8888</td>
+                                    <td><a href="mailto:x@x.com">j.doe@exampleserver.com</a></td>
+                                    <td>15/02/1971</td>
+                                </tr>
+                                <tr>
+                                    <td>John</td>
+                                    <td>Johnson</td>
+                                    <td>(04) 7777 7777</td>
+                                    <td><a href="mailto:x@x.com">jj@jayjayjay.com</a></td>
+                                    <td>12/10/1965</td>
+                                </tr>
+                                <tr>
+                                    <td>Tracy</td>
+                                    <td>Richardson</td>
+                                    <td>(02) 8888 8888</td>
+                                    <td><a href="mailto:x@x.com">trichard@website.com</a></td>
+                                    <td>03/11/1980</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
