b4e668d17ecf661a4394158bf8356d6ea676d9f6
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-19 14:14:28 +1000

    Refactored logic for spacing notification banner

--- src/scripts/breadcrumbs.ts
+++ src/scripts/breadcrumbs.ts
@@ -2,28 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const notification = document.querySelector('.notification');
     const breadcrumbs = document.querySelector('.breadcrumbs');
 
-    // If notification is in page
+    // If notification banner is on the page
     if (notification && notification.previousSibling) {
 
-        // Get the previoius sibling to notification
-        const previousElement = notification.previousElementSibling;
-
-        // Check if the previous element is breadcrumbs,
-        // if so set the top position of notification to take that in to account, otherwise set top position to just below the nav height.
-        if (previousElement) {
-            if (previousElement.contains(breadcrumbs)) {
-                // 4rem for nav + 2rem for breadcrumbs
-                notification.setAttribute("style", "top: 6rem");
-            }
-            else {
-                // 4rem for nav
-                notification.setAttribute("style", "top: 4rem");
-
-            }
+        // Check if breadcrumbs exist on the page, if so set the top position of notification banner to take that in to account.
+        if (breadcrumbs) {
+            // 4rem for nav + 2rem for breadcrumbs
+            notification.setAttribute("style", "top: 6rem");
+            //otherwise set top position to just below the nav height (4rem).
+        } else {
+            // 4rem for nav
+            notification.setAttribute("style", "top: 4rem");
         }
     }
-
-
 }, false);
 
 export default {};
