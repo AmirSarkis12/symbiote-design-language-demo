7f4dbac53f9f4ccba2fb9bfc2b41e7978c4b87a4
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-26 21:53:43 +1000

    Added Radio documentation

--- src/components/Philosophy.tsx
+++ src/components/Philosophy.tsx
@@ -4,7 +4,16 @@ function Philosophy() {
     return (
         <div className="container">
             <div className="col-12">
-                <h2>Philosophy</h2>
+                <h1>Philosophy</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum neque eget nunc ornare aliquam. Nunc eget egestas enim. Vestibulum pulvinar sit amet nisl eu dictum. Aenean suscipit eros sapien, vel tristique mauris imperdiet non. Nulla finibus leo eget vehicula efficitur. Integer scelerisque purus dui, vitae molestie eros pharetra at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse non bibendum urna, ut rutrum arcu.</p>
+
+                <p>Mauris tempor blandit fringilla. Fusce ornare semper justo nec bibendum. Praesent congue diam eu libero imperdiet dapibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut volutpat efficitur laoreet. Integer faucibus ex et odio vestibulum pulvinar. Nullam convallis ultricies diam, in luctus est semper et. Integer gravida, neque in bibendum congue, nisi lacus consectetur metus, eu elementum enim erat in dolor. Donec aliquam commodo eros vitae semper. Duis et orci sed nulla egestas facilisis vel ut leo. Vivamus sollicitudin efficitur interdum.</p>
+
+                <p>Sed eu nisi laoreet, rutrum orci a, dignissim nisl. Nulla luctus eleifend dui at egestas. Etiam convallis pretium odio, vel ullamcorper massa posuere non. Nullam a lorem leo. Integer a scelerisque neque, et placerat leo. Integer a vehicula sapien, at vulputate ante. Maecenas mollis, quam non feugiat pretium, arcu erat varius ante, a dictum neque ipsum nec urna. Maecenas sit amet risus at lacus suscipit imperdiet. Sed pretium felis in purus ultricies tempus. Aliquam dapibus, felis quis tempor mattis, magna lacus facilisis mi, non gravida lorem massa vel elit. Fusce tristique orci ut justo sodales, vitae iaculis sapien cursus.</p>
+
+                <p>Aliquam consequat vestibulum nunc ut cursus.Cras et vulputate libero, quis elementum eros.Vestibulum volutpat lacus condimentum rutrum bibendum.Donec sed lacus ullamcorper neque hendrerit convallis a ac nisl.Duis in justo nec massa dapibus rutrum.Praesent dapibus viverra tellus ac interdum.Maecenas tempus, augue id vestibulum consequat, arcu felis aliquet dolor, at mattis turpis lacus at lectus.In a commodo odio, non molestie metus.Sed id aliquet augue.Pellentesque faucibus, diam in scelerisque aliquet, tellus enim dignissim elit, vitae consectetur nisl velit vitae purus.Maecenas feugiat eros quis blandit ultricies.Cras at ornare orci, eu semper nisi.Nulla facilisi.</p >
+
+                <p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent id commodo ante. Praesent tristique dui sapien, quis venenatis risus consectetur faucibus. In in orci condimentum, feugiat neque vel, varius ipsum. Praesent efficitur justo eget massa lacinia fermentum. Praesent ut nunc porttitor, viverra nisl nec, lobortis tortor. Quisque sodales suscipit convallis.</p>
             </div>
         </div>
     )
--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -1,18 +1,124 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Prism from 'prismjs'
 
 function Radio() {
+    // React hook to get syntax highlighing. Similar to componentDidMount()
+    useEffect(() => {
+        Prism.highlightAll();
+    });
+
+    const code1 =
+`<p>What is your favourite color?</p>
+
+<div className="radio">
+    <p className="title">Blue</p>
+    <label htmlFor="radio1" className="radio-label">
+        <div className="radio-group">
+            <input type="radio" className="radio-input" id="radio1" name="customRadio" hidden />
+            <span className="radio-checked"></span>
+        </div>
+    </label>
+    <p className="title">Red</p>
+    <label htmlFor="radio2" className="radio-label">
+        <div className="radio-group">
+            <input type="radio" className="radio-input" id="radio2" name="customRadio" hidden />
+            <span className="radio-checked"></span>
+        </div>
+    </label>
+    <p className="title">Yellow</p>
+    <label htmlFor="radio3" className="radio-label">
+        <div className="radio-group">
+            <input type="radio" className="radio-input" id="radio3" name="customRadio" hidden />
+            <span className="radio-checked"></span>
+        </div>
+    </label>
+    <p className="title">Green</p>
+    <label htmlFor="radio4" className="radio-label">
+        <div className="radio-group">
+            <input type="radio" className="radio-input" id="radio4" name="customRadio" hidden />
+            <span className="radio-checked"></span>
+        </div>
+    </label>
+</div>`;
+
     return (
         <div>
             <h1>Radio</h1>
-            <p></p>
+            <div className="radio">
+                <label htmlFor="radio1" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio1" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+                <label htmlFor="radio2" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio2" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+                <label htmlFor="radio3" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio3" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+                <label htmlFor="radio4" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio4" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+            </div>
+            <p>Radio buttons display a set of mutually exclusive options from which a user must select exactly one.</p>
 
             <h3>When to use this component</h3>
-            <p></p>
+            <p>Radio buttons should be used when an option in a given option list is required and therefore an option should be selected by default.</p>
 
             <h3>When not to use this component</h3>
-            <p></p>
+            <p>Do not use Radio buttons when the list of options is not mutually exclusive. It should be clear to the user that selecting one option will anull the other options.</p>
+
+            <h3>Snippet</h3>
+
+            <p>What is your favourite color?</p>
+            <div className="radio">
+                <p className="title">Blue</p>
+                <label htmlFor="radio1" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio1" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+                <p className="title">Red</p>
+                <label htmlFor="radio2" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio2" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+                <p className="title">Yellow</p>
+                <label htmlFor="radio3" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio3" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+                <p className="title">Green</p>
+                <label htmlFor="radio4" className="radio-label">
+                    <div className="radio-group">
+                        <input type="radio" className="radio-input" id="radio4" name="customRadio" hidden />
+                        <span className="radio-checked"></span>
+                    </div>
+                </label>
+            </div>
+            <pre>
+                <code className="language-javascript">
+                    {code1}
+                </code>
+            </pre>
+
         </div>
     )
-    }
+}
 
 export default Radio
