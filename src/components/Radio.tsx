0d3f8da9d746f786bbbbb668c5d747e5aac547a8
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-26 22:14:31 +1000

    Added Select component documentation

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -11,65 +11,63 @@ function Radio() {
 `<p>What is your favourite color?</p>
 
 <div className="radio">
-    <p className="title">Blue</p>
-    <label htmlFor="radio1" className="radio-label">
+    <p className="title">Red</p>
+    <label htmlFor="radioRed" className="radio-label">
         <div className="radio-group">
-            <input type="radio" className="radio-input" id="radio1" name="customRadio" hidden />
+            <input type="radio" className="radio-input" id="radioRed" name="radioColor" hidden />
             <span className="radio-checked"></span>
         </div>
     </label>
-    <p className="title">Red</p>
-    <label htmlFor="radio2" className="radio-label">
+    <p className="title">Blue</p>
+    <label htmlFor="radioBlue" className="radio-label">
         <div className="radio-group">
-            <input type="radio" className="radio-input" id="radio2" name="customRadio" hidden />
+            <input type="radio" className="radio-input" id="radioBlue" name="radioColor" hidden />
             <span className="radio-checked"></span>
         </div>
     </label>
     <p className="title">Yellow</p>
-    <label htmlFor="radio3" className="radio-label">
+    <label htmlFor="radioYellow" className="radio-label">
         <div className="radio-group">
-            <input type="radio" className="radio-input" id="radio3" name="customRadio" hidden />
+            <input type="radio" className="radio-input" id="radioYellow" name="radioColor" hidden />
             <span className="radio-checked"></span>
         </div>
     </label>
     <p className="title">Green</p>
-    <label htmlFor="radio4" className="radio-label">
+    <label htmlFor="radioGreen" className="radio-label">
         <div className="radio-group">
-            <input type="radio" className="radio-input" id="radio4" name="customRadio" hidden />
+            <input type="radio" className="radio-input" id="radioGreen" name="radioColor" hidden />
             <span className="radio-checked"></span>
         </div>
     </label>
-</div>`;
+    </div>`;
 
     return (
         <div>
             <h1>Radio</h1>
             <div className="radio">
-                <label htmlFor="radio1" className="radio-label">
+                <p className="title">Beef</p>
+                <label htmlFor="radioBeef" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio1" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioBeef" name="radioKebab" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
-                <label htmlFor="radio2" className="radio-label">
+                <p className="title">Chicken</p>
+                <label htmlFor="radioChicken" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio2" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioChicken" name="radioKebab" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
-                <label htmlFor="radio3" className="radio-label">
+                <p className="title">Lamb</p>
+                <label htmlFor="radioLamb" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio3" name="customRadio" hidden />
-                        <span className="radio-checked"></span>
-                    </div>
-                </label>
-                <label htmlFor="radio4" className="radio-label">
-                    <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio4" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioLamb" name="radioKebab" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
             </div>
+
             <p>Radio buttons display a set of mutually exclusive options from which a user must select exactly one.</p>
 
             <h3>When to use this component</h3>
@@ -82,31 +80,31 @@ function Radio() {
 
             <p>What is your favourite color?</p>
             <div className="radio">
-                <p className="title">Blue</p>
-                <label htmlFor="radio1" className="radio-label">
+                <p className="title">Red</p>
+                <label htmlFor="radioRed" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio1" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioRed" name="radioColor" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
-                <p className="title">Red</p>
-                <label htmlFor="radio2" className="radio-label">
+                <p className="title">Blue</p>
+                <label htmlFor="radioBlue" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio2" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioBlue" name="radioColor" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
                 <p className="title">Yellow</p>
-                <label htmlFor="radio3" className="radio-label">
+                <label htmlFor="radioYellow" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio3" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioYellow" name="radioColor" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
                 <p className="title">Green</p>
-                <label htmlFor="radio4" className="radio-label">
+                <label htmlFor="radioGreen" className="radio-label">
                     <div className="radio-group">
-                        <input type="radio" className="radio-input" id="radio4" name="customRadio" hidden />
+                        <input type="radio" className="radio-input" id="radioGreen" name="radioColor" hidden />
                         <span className="radio-checked"></span>
                     </div>
                 </label>
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,18 +1,104 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Prism from 'prismjs'
 
 function Select() {
+    // React hook to get syntax highlighing. Similar to componentDidMount()
+    useEffect(() => {
+        Prism.highlightAll();
+    });
+
+    const code1 =
+        `<form>
+    <label htmlFor="shipping">Please select your shipping option</label>
+    <select name="shipping">
+        <option value="">AirMail</option>
+        <option value="">AirMail Standard </option>
+        <option value="">Ground Shipping</option>
+        <option value="">Expedited</option>
+        <option value="">Registered Ground</option>
+        <option value="">Express</option>
+        <option value="">Expressed Registered</option>
+        <option value="">Snail Mail</option>
+        <option value="">Courier</option>
+        <option value="">Courier Express</option>
+        <option value="">Registered Courier</option>
+        <option value="">Pick-up</option>
+        <option value="">After hours Pick-up</option>
+    </select>
+</form>`;
+
     return (
         <div>
             <h1>Select</h1>
-            <p></p>
+            <form>
+                <label htmlFor="shipping">What is your favourite letter of the alphabet?</label>
+                <select name="shipping">
+                    <option value="">A</option>
+                    <option value="">B</option>
+                    <option value="">C</option>
+                    <option value="">D</option>
+                    <option value="">E</option>
+                    <option value="">F</option>
+                    <option value="">G</option>
+                    <option value="">H</option>
+                    <option value="">I</option>
+                    <option value="">J</option>
+                    <option value="">K</option>
+                    <option value="">L</option>
+                    <option value="">M</option>
+                    <option value="">N</option>
+                    <option value="">O</option>
+                    <option value="">P</option>
+                    <option value="">Q</option>
+                    <option value="">R</option>
+                    <option value="">S</option>
+                    <option value="">T</option>
+                    <option value="">U</option>
+                    <option value="">V</option>
+                    <option value="">W</option>
+                    <option value="">X</option>
+                    <option value="">Y</option>
+                    <option value="">Z</option>
+                </select>
+            </form>
+
+            <p>The Select component allows users to choose an option from a long list.</p>
 
             <h3>When to use this component</h3>
-            <p></p>
+            <p>This component should only be used when a long list of options is being presented.</p>
 
             <h3>When not to use this component</h3>
-            <p></p>
+            <p>It is not recommended to use this component when only a short list of options are presented. Some users may have difficulty with Select components and as such a group of radio buttons may suffice in order to reduce overall complexity.</p>
+
+            <h3>Snippet</h3>
+
+            <form>
+                <label htmlFor="shipping">Please select your shipping option</label>
+                <select name="shipping">
+                    <option value="">AirMail</option>
+                    <option value="">AirMail Standard </option>
+                    <option value="">Ground Shipping</option>
+                    <option value="">Expedited</option>
+                    <option value="">Registered Ground</option>
+                    <option value="">Express</option>
+                    <option value="">Expressed Registered</option>
+                    <option value="">Snail Mail</option>
+                    <option value="">Courier</option>
+                    <option value="">Courier Express</option>
+                    <option value="">Registered Courier</option>
+                    <option value="">Pick-up</option>
+                    <option value="">After hours Pick-up</option>
+                </select>
+            </form>
+
+            <pre>
+                <code className="language-javascript">
+                    {code1}
+                </code>
+            </pre>
+
         </div>
     )
-    }
+}
 
 export default Select
