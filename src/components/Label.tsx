a59abcd978938a05033279d28c277cfc9abb238d
Amir Sarkis <sarkis.amir@gmail.com>
2020-09-28 12:59:52 +1000

    Added documentation for Tooltip and Label components

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -8,20 +8,29 @@ function Label() {
     });
 
     const code1 =
-        ``;
+        `<span className="label label-success">Success</span>
+<span className="label label-warning">Warning</span>
+<span className="label label-error">Error</span>
+<span className="label label-new">New</span>
+<span className="label label-info">Info</span>`;
 
     return (
         <div>
             <h1>Label</h1>
-            <p></p>
+            <p>This component is used to notify the user when a something has occured, for example, when a new feature has been made available or if an error has occured.</p>
 
             <h3>When to use this component</h3>
-            <p></p>
+            <p>Use this component sparingly and only when a small piece of text will be enough to relay that something requires attention.</p>
 
             <h3>When not to use this component</h3>
-            <p></p>
+            <p>If the event that has occured requires more text than a single word consider using a Notification or Page Alert component instead.</p>
 
             <h3>Snippet</h3>
+            <span className="label label-success">Success</span>
+            <span className="label label-warning">Warning</span>
+            <span className="label label-error">Error</span>
+            <span className="label label-new">New</span>
+            <span className="label label-info">Info</span>
 
             <pre>
                 <code className="language-javascript">
--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -8,20 +8,27 @@ function Tooltip() {
     });
 
     const code1 =
-        ``;
+        `<div className="tooltip">
+    <p>Hover over me</p>
+    <span className="tooltip-text">Hey, I'm a tooltip!</span>
+</div>`;
 
     return (
         <div>
             <h1>Tooltip</h1>
-            <p></p>
+            <p>The Tooltip component is a text labels that appears when a user hovers over, focuses on, or touches an element.</p>
 
             <h3>When to use this component</h3>
-            <p></p>
+            <p>This component should be used sparingly and only when a specific item may need further clasrification</p>
 
             <h3>When not to use this component</h3>
-            <p></p>
+            <p>Do not use this component for common UI elements that do not require any further information.</p>
 
             <h3>Snippet</h3>
+            <div className="tooltip">
+                <p>Hover over me</p>
+                <span className="tooltip-text">Hey, I'm a tooltip!</span>
+            </div>
 
             <pre>
                 <code className="language-javascript">
